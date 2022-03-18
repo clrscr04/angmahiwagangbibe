f4e5b5f1403d8c42ddb84008e17e4a92ebb0c1d4
clrscr04 <81803870+clrscr04@users.noreply.github.com>
2022-03-18 20:53:00 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,198 @@
+$(document).ready(function(){
+
+  let dataToAdd=[];
+
+
+$("#printQuote").on("click",function(){
+  var printContents = document.getElementById("printableDIV").innerHTML;
+var originalContents = document.body.innerHTML;
+document.body.innerHTML = printContents;
+window.print();
+document.body.innerHTML = originalContents;
+});
+
+
+
+$("#updateQuote").on("click", function(){
+$("#cNameToPrint").text($("#clientName").val());
+$("#cInfoToPrint").text($("#clientContactInfo").val());
+$("#dateToPrint").text($("#Date").val());
+$("#RfqToPrint").text($("#rfqNumber").val());
+$("#nameInfo").text($("#empName").val());
+$("#position69").text($("#empPos").val());
+});
+
+
+
+
+
+
+
+
+$(".editableDIV").each(function(){
+  this.contentEditable = true;
+});
+
+$("#logoUpload").change(function() {
+  const file = this.files[0];
+  console.log(file);
+  if (file) {
+    let reader = new FileReader();
+    reader.onload = function(event) {
+      console.log(event.target.result);
+      $('#logoImage').attr('src', event.target.result);
+    }
+    reader.readAsDataURL(file);
+  }
+});
+
+showData();
+
+$("#addUnit").on("click", function() {
+  event.preventDefault();
+  //compute price and discount depending on quantity
+  let subTotal = 0;
+  let priceDiscount = Number($("#pDiscount").val());
+  let productPrice = Number($("#pPrice").val());
+  let productQuantity = Number($("#pQuantity").val());
+  if (priceDiscount == "" || priceDiscount == 0) {
+    priceDiscount = 0;
+    subTotal = productPrice * productQuantity;
+  } else {
+    subTotal = priceDiscount * productQuantity;
+  }
+  //create oblject
+  let unitObject = {
+    Quantity: productQuantity,
+    pName: $("#pName").val(),
+    pDesc: $("#pSpecs").val(),
+    pPrice: productPrice,
+    pDiscount: priceDiscount,
+    unitSubTotal: subTotal
+  };
+  //push/add object to array
+  dataToAdd.push(unitObject);
+  //redisplay data with updated arraylist
+  showData();
+
+  //clear fields
+  $("#pQuantity").val(1);
+  $("#pName").val("");
+  $("#pCode").val("");
+  $("#pDesc").val("");
+  $("#pPrice").val("");
+  $("#pDiscount").val("");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//the function we need, but not deserve
+function showData() {
+  $("#itemList").DataTable({
+    "footerCallback": function ( row, data, start, end, display ) {
+        var api = this.api();
+        // Remove the formatting to get integer data for summation
+        var intVal = function ( i ) {
+            return typeof i === 'string' ?
+                i.replace(/[\$,]/g, '')*1 :
+                typeof i === 'number' ?
+                    i : 0;
+        };
+        // Total over all pages
+        total = api
+            .column( 5 )
+            .data()
+            .reduce( function (a, b) {
+                return intVal(a) + intVal(b);
+            }, 0 );
+        // Total over this page
+        pageTotal = api
+            .column( 5, { page: 'current'} )
+            .data()
+            .reduce( function (a, b) {
+                return intVal(a) + intVal(b);
+            }, 0 );
+        // Update footer
+        $( api.column( 5 ).footer() ).html(
+            'P'+pageTotal +' ( P'+ total +' total)'
+        );
+    },
+    searching: false,
+    info: false,
+    paging: false,
+    destroy: true,
+    pageLength: 10,
+    data: dataToAdd,
+    columns: [{
+        data: "Quantity"
+      },
+      {
+        data: "pName"
+      },
+      {
+        data: "pDesc"
+      },
+      {
+        data: "pPrice"
+      },
+      {
+        data: "pDiscount"
+      },
+      {
+        data: "unitSubTotal"
+      }
+    ]
+  });
+};
+
+
+
+
+
+//delete the row selected on click
+  $('#itemList tbody').on('click', 'tr', function() {
+    let tableNew = $('#itemList').DataTable();
+    if ($(this).hasClass('selected')) {
+      $(this).removeClass('selected');
+    } else {
+      tableNew.$('tr.selected').removeClass('selected');
+      $(this).addClass('selected');
+    }
+    tableNew.row('.selected').remove().draw(false);
+  });
+
+
+
+
+
+
+});
